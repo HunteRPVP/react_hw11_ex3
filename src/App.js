e8445535ffd4e4bf162bd46b6f7c5e88dc31651a
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-06-27 21:55:41 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import TodoList from "./TodoList";
+import { Context } from "./context";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const handleClick = () => console.log("click");
+
+  useEffect(() => {
+    const raw = localStorage.getItem("todos") || [];
+    setTodos(JSON.parse(raw));
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [todos]);
+
+  const addTodo = (event) => {
+    if (event.key === "Enter") {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: todoTitle,
+          completed: false,
+        },
+      ]);
+      setTodoTitle("");
+    }
+  };
+
+  const removeTodo = (id) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== id;
+      })
+    );
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        toggleTodo,
+        removeTodo,
+      }}
+    >
+      <div className="container">
+        <h1>Todo app</h1>
+
+        <div className="input-field">
+          <input
+            type="text"
+            value={todoTitle}
+            onChange={(event) => setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}
+          />
+          <label className="active">Todo name</label>
+        </div>
+
+        <TodoList todos={todos} />
+      </div>
+    </Context.Provider>
+  );
+}
